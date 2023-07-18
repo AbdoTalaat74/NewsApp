384ec6647502d670e926f99848bcb54c7bf4cb6c
AbdoTalaat74 <abdo.talaat740@gmail.con>
2023-07-18 17:40:06 +0300

    setUp Retrofit

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "6ec73b33545c40b28153c3e33b4ec853"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
